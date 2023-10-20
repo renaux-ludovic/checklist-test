142af72a0f41a1d6cfa7dd9880c8f88204ceb275
Ludovic Renaux <renauxludovic77@gmail.com>
2023-10-20 15:51:42 +0200

    add checklist url

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,23 +1,22 @@
-const CACHE_NAME = 'my-cache';
+const CACHE_NAME = "my-cache";
 
 const urlsToCache = [
-  '/',
+  "/",
+  "/checklist",
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
