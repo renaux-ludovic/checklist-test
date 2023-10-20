7f714864fd25b5bc3e415662c2d94b0f87d60882
Ludovic Renaux <renauxludovic77@gmail.com>
2023-10-20 15:27:02 +0200

    test pwa deploy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/service-worker.js')
@@ -16,15 +21,10 @@ if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
   });
 }
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorkerRegistration.register();
