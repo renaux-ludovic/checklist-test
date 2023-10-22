78ab2a0869e0329b391b5ed181f55fc6342c301a
Ludovic Renaux <renauxludovic77@gmail.com>
2023-10-22 07:45:51 +0200

    404 try fix 2

--- src/App.js
+++ src/App.js
@@ -60,7 +60,8 @@
 // export default App;
 
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Menu from "./pages/menu";
 import List1 from "./pages/list1";
 import List2 from "./pages/list2";
@@ -120,7 +121,7 @@ function App() {
       ) : window.deferredPrompt ? null : (
         <Router>
           <Routes>
-            <Route path="/checklist-test/*" element={<Menu />} />
+            <Route path="/checklist-test/" element={<Menu />} />
             <Route path="/checklist-test/list-1" element={<List1 />} />
             <Route path="/checklist-test/list-2" element={<List2 />} />
             <Route path="/checklist-test/list-3" element={<List3 />} />
