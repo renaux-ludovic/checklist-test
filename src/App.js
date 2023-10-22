c1c6beb58500f38c326ab709b860aa81e9c36f6f
Ludovic Renaux <renauxludovic77@gmail.com>
2023-10-22 09:18:33 +0200

    404 try fix 6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-// import React, { useEffect, useState } from 'react';
+
+// import React, { useEffect, useState } from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import Menu from "./pages/menu";
+// import List1 from "./pages/list1";
+// import List2 from "./pages/list2";
+// import List3 from "./pages/list3";
+// import List4 from "./pages/list4";
+
 
 // function App() {
 //   const [buttonVisible, setButtonVisible] = useState(false);
@@ -8,16 +15,15 @@
 //   const handleInstallClick = () => {
 //     if (window.deferredPrompt) {
 //       window.deferredPrompt.prompt();
-//       window.deferredPrompt.userChoice
-//         .then((choiceResult) => {
-//           if (choiceResult.outcome === 'accepted') {
-//             console.log("L'utilisateur a accepté l'installation.");
-//           } else {
-//             console.log("L'utilisateur a refusé l'installation.");
-//           }
-//           window.deferredPrompt = null;
-//           setButtonVisible(false);
-//         });
+//       window.deferredPrompt.userChoice.then((choiceResult) => {
+//         if (choiceResult.outcome === "accepted") {
+//           console.log("L'utilisateur a accepté l'installation.");
+//         } else {
+//           console.log("L'utilisateur a refusé l'installation.");
+//         }
+//         window.deferredPrompt = null;
+//         setButtonVisible(false);
+//       });
 //     }
 //   };
 
@@ -38,7 +44,10 @@
 //     }, 2000);
 
 //     return () => {
-//       window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+//       window.removeEventListener(
+//         "beforeinstallprompt",
+//         handleBeforeInstallPrompt
+//       );
 //     };
 //   }, []);
 
@@ -46,12 +55,18 @@
 //     <div className="App">
 //       {loading ? (
 //         <p>Chargement en cours...</p>
-//       ) : (
-//         buttonVisible ? (
-//           <button onClick={handleInstallClick}>Installer</button>
-//         ) : (
-//           window.deferredPrompt ? null : <Menu />
-//         )
+//       ) : buttonVisible ? (
+//         <button onClick={handleInstallClick}>Installer</button>
+//       ) : window.deferredPrompt ? null : (
+//         <Router>
+//           <Routes>
+//             <Route path="/checklist-test/*" element={<Menu />} />
+//             <Route path="/checklist-test/list-1" element={<List1 />} />
+//             <Route path="/checklist-test/list-2" element={<List2 />} />
+//             <Route path="/checklist-test/list-3" element={<List3 />} />
+//             <Route path="/checklist-test/list-4" element={<List4 />} />
+//           </Routes>
+//         </Router>
 //       )}
 //     </div>
 //   );
@@ -59,9 +74,9 @@
 
 // export default App;
 
-import React, { useEffect, useState } from "react";
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Menu from "./pages/menu";
 import List1 from "./pages/list1";
 import List2 from "./pages/list2";
@@ -118,15 +133,15 @@ function App() {
       ) : buttonVisible ? (
         <button onClick={handleInstallClick}>Installer</button>
       ) : window.deferredPrompt ? null : (
-        <Router>
+        <BrowserRouter basename="/checklist-test">
           <Routes>
-            <Route path="/checklist-test" element={<Menu />} />
-            <Route path="/checklist-test/list-1" element={<List1 />} />
-            <Route path="/checklist-test/list-2" element={<List2 />} />
-            <Route path="/checklist-test/list-3" element={<List3 />} />
-            <Route path="/checklist-test/list-4" element={<List4 />} />
+            <Route exact path="/checklist-test" element={<Menu />} />
+            <Route path="/list-1" element={<List1 />} />
+            <Route path="/list-2" element={<List2 />} />
+            <Route path="/list-3" element={<List3 />} />
+            <Route path="/list-4" element={<List4 />} />
           </Routes>
-        </Router>
+        </BrowserRouter>
       )}
     </div>
   );
